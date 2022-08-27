d0f2d633a53d27ea018feca6ca6008e8e4aa7793
wesley <sunbeyond97@163.com>
2022-08-27 17:50:24 +0800

    Site updated: 2022-08-27 17:50:23

--- scripts/page.js
+++ scripts/page.js
@@ -1,3 +1,4 @@
 hexo.extend.filter.register('theme_inject', function(injects) {
   injects.header.file('video-banner', 'source/_inject/header.ejs', { key: 'value' }, -1);
+  injects.footer.raw('default', '<script async defer data-website-id="dcb701b2-2067-41e4-9ee4-24826173cd35" src="https://wesley-umami.up.railway.app/vue.js"></script>');
 });
