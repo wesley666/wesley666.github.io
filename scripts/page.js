6e300bd9ae0c8b7428330eb39802807654f1944e
wesley <sunbeyond97@163.com>
2022-08-27 18:08:07 +0800

    Site updated: 2022-08-27 18:08:07

--- scripts/page.js
+++ scripts/page.js
@@ -1,4 +1,4 @@
 hexo.extend.filter.register('theme_inject', function(injects) {
   injects.header.file('video-banner', 'source/_inject/header.ejs', { key: 'value' }, -1);
-  injects.footer.raw('default', '<script async defer data-website-id="dcb701b2-2067-41e4-9ee4-24826173cd35" src="https://wesley-umami.up.railway.app/vue.js"></script>');
+  injects.footer.raw('default-a', '<script async defer data-website-id="435eb2a5-f5a8-4d9f-96fa-1f961328aa51" src="https://umami-wesley.up.railway.app/vue.js"></script>');
 });
